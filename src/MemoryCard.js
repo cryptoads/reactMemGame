31cdf0769c6fc15a4ddac9e024885bd532fed041
Chris Michels <chrisjohnmichels@gmail.com>
2018-06-28 20:03:39 -0400

    flipping cards

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -1,12 +1,31 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import App from './App';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    constructor(){
+        super();
+        this.state = {isFlipped: false};
+    }
     render() {
+        let clickHandler=()=>{
+            this.setState({isFlipped: !this.state.isFlipped})
+        };
+
+        let memoryCardInnerClass = "MemoryCardInner";
+
+        if(this.state.isFlipped){
+            memoryCardInnerClass += " flipped";
+        }
         return (
-            <div className="MemoryCard">
-                   <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" />                 
+            <div className="MemoryCard" onClick={clickHandler}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" /></div> 
+                    <div className="MemoryCardFront">
+                      ∆
+                    </div>
+                </div>
             </div>)
     }
 }
