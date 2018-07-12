0a8741d10ef2770ae27f2f917c67865a57e7b7f8
Chris Michels <chrisjohnmichels@gmail.com>
2018-07-11 21:22:02 -0400

    adding react-router-dom

---
+++ src/Ajax.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Ajax extends Component {
+    render(){
+        return<h1>Ajax</h1>;
+    }
+}
+
+export default Ajax; 
--- src/App.js
+++ src/App.js
@@ -1,109 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import MemoryCard from './MemoryCard.js';
-
-
-let generateDeck = () =>{
-  let symbols = ["∆", "ß", "£", "§", "•", "$", "+", "ø"];
-  let deck = [];
-  for(var i =0; i<16; i++){
-    deck.push({isFlipped: false, symbol: symbols[i%8]})
-  }
-  shuffle(deck);
-  return(deck);
-};
-
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-};
+import { Link, Route, Switch } from 'react-router-dom'; 
+import MemoryGame from './MemoryGame.js';
+import Ajax from './Ajax.js';
 
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {deck: generateDeck(), pickedCards: []};
-
-  }
-
-
-  pickCard(cardIndex){
-
-    if(this.state.deck[cardIndex].isFlipped){
-      return;
+    constructor(){
+        super()
     }
-    let cardToFlip = {...this.state.deck[cardIndex]}
-    cardToFlip.isFlipped = true;
- 
-    let newPickedCards = this.state.pickedCards.concat(cardIndex);
-    let newDeck = this.state.deck.map((card, index)=>{
-      if(cardIndex == index){
-        return cardToFlip;
-      }
-      return card;
-    })
 
-    if(newPickedCards.length == 2){
-      let card1Index = newPickedCards[0];
-      let card2Index = newPickedCards[1];
-      if(newDeck[card1Index].symbol !== newDeck[card2Index].symbol){
-        setTimeout(this.unflipCards.bind(this,card1Index, card2Index),1000)
-      }
-      newPickedCards = []
-    }
-    this.setState({deck: newDeck, pickedCards:newPickedCards});
-  }
-
-  unflipCards(card1Index, card2Index){
-        let card1 = {...this.state.deck[card1Index]}
-        let card2 = {...this.state.deck[card2Index]}
-        card1.isFlipped = false;
-        card2.isFlipped = false;
-        var newDeck = this.state.deck.map((card, index)=>{
-          if(card1Index == index){
-            return card1;
-          }else if (card2Index == index) {
-            return card2;
-          }
-          return card;
-        
-        })
-        console.log(newDeck)
-      this.setState({deck: newDeck});
-  }
-
-
-  render() {
-
-    let cardsJSX = this.state.deck.map((card, index) => {
-      return <MemoryCard symbol={card.symbol} isFlipped={card.isFlipped} key={index} pickCard={this.pickCard.bind(this,index)} />
-    });
+render(){
     return (
-      <div className="App">
-        <header className="App-header" style = {{height: 80}}>
-
-          <h1 className="App-title">Memory Game</h1>
-          <h3 className="App-subTitle" style={{color: '#666666'}}> Match cards to win</h3>
-        </header>
         <div>
-          {cardsJSX.slice(0,4)}
+        <div className="navbar">
+            <Link to="/memory">Memory Game</Link>
+            <Link to="/ajax">Ajax</Link>
         </div>
-        <div>
-          {cardsJSX.slice(4,8)}
+        <Switch>
+            <Route exact path="/" component={MemoryGame} />
+            <Route path="/memory" component={MemoryGame} />
+            <Route path="/ajax" component={Ajax} />
+        </Switch>
         </div>
-        <div>
-          {cardsJSX.slice(8,12)}
-        </div>
-        <div>
-          {cardsJSX.slice(12,16)}
-        </div>
-      </div>
-    );
-  }
+        )
 }
 
+}
 
 export default App;
